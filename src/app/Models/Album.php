f11a0a1fd004cfbf8745d992e521ce9135ae8605
Dan-Sones <daniel.sones123@gmail.com>
2024-11-21 14:34:04 +0000

    Add Song Model, Add tracklist

--- src/app/Models/Album.php
+++ src/app/Models/Album.php
@@ -4,14 +4,15 @@ namespace S246109\BeatMagazine\Models;
 
 class Album
 {
-    public $albumArt;
-    public $albumName;
-    public $artistName;
-    public $genre;
-    public $label;
-    public $averageUserRating;
-    public $journalistRating;
-    public $releaseDate;
+    private $albumArt;
+    private $albumName;
+    private $artistName;
+    private $genre;
+    private $label;
+    private $averageUserRating;
+    private $journalistRating;
+    private $releaseDate;
+    private $songs;
 
     /**
      * @param $albumArt
@@ -22,7 +23,7 @@ class Album
      * @param $averageUserRating
      * @param $journalistRating
      */
-    public function __construct($albumArt, $albumName, $artistName, $genre, $label, $averageUserRating, $journalistRating, $releaseDate)
+    public function __construct($albumArt, $albumName, $artistName, $genre, $label, $averageUserRating, $journalistRating, $releaseDate, $songs)
     {
         $this->albumArt = $albumArt;
         $this->albumName = $albumName;
@@ -32,6 +33,79 @@ class Album
         $this->averageUserRating = $averageUserRating;
         $this->journalistRating = $journalistRating;
         $this->releaseDate = $releaseDate;
+        $this->songs = $songs;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSongs()
+    {
+        return $this->songs;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAlbumArt()
+    {
+        return $this->albumArt;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAlbumName()
+    {
+        return $this->albumName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getArtistName()
+    {
+        return $this->artistName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAverageUserRating()
+    {
+        return $this->averageUserRating;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getJournalistRating()
+    {
+        return $this->journalistRating;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getReleaseDate()
+    {
+        return $this->releaseDate;
     }
 
 
--- src/app/Views/album.php
+++ src/app/Views/album.php
@@ -10,31 +10,33 @@
 
                             <div class="row album-content">
                                 <div class="col-12 col-lg-4 order-1 order-lg-1 align-content-center">
-                                    <img src="<?= htmlspecialchars($album->albumArt) ?>" class="img-fluid shadow-sm"
+                                    <img src="<?= htmlspecialchars($album->getAlbumArt()) ?>"
+                                         class="img-fluid shadow-sm"
                                          alt="Responsive Image">
                                 </div>
 
                                 <div class="album-info col-12 col-lg-8 ps-4 order-2 order-lg-2">
                                     <div class="row title-artist-section">
                                         <div class="row">
-                                            <h2><?= $album->albumName ?> </h2>
+                                            <h2><?= $album->getAlbumName() ?> </h2>
                                         </div>
                                         <div class="row">
-                                            <h5><?= $album->artistName ?></h5>
+                                            <h5><?= $album->getArtistName() ?></h5>
                                         </div>
                                     </div>
                                     <div class="row info d-flex align-items-center">
-                                        <p><strong>Release Date:</strong> <?= $album->releaseDate ?></p>
-                                        <p><strong>Genre</strong>: <?= $album->genre ?></p>
-                                        <p><strong>Label:</strong> <?= $album->label ?></p>
+                                        <p><strong>Release Date:</strong> <?= $album->getReleaseDate() ?></p>
+                                        <p><strong>Genre</strong>: <?= $album->getGenre() ?></p>
+                                        <p><strong>Label:</strong> <?= $album->getLabel() ?></p>
                                     </div>
                                     <div class="row d-flex align-items-center">
                                         <div class="col-6 d-flex align-content-center-center">
-                                            <p><strong>Average User Rating:</strong> <?= $album->averageUserRating ?>
-                                            </p>
+                                            <p><strong>Average User
+                                                    Rating:</strong> <?= $album->getAverageUserRating() ?></p>
                                         </div>
                                         <div class="col-6 d-flex align-content-center">
-                                            <p><strong>Journalist Rating:</strong> <?= $album->journalistRating ?></p>
+                                            <p><strong>Journalist Rating:</strong> <?= $album->getJournalistRating() ?>
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
@@ -49,9 +51,23 @@
                                 </div>
                                 <!-- Content to Toggle -->
                                 <ul id="tracklist" class="mt-3" style="display: none;">
-                                    <li>Track 1</li>
-                                    <li>Track 2</li>
-                                    <li>Track 3</li>
+                                    <div class="recent-tracks-section">
+                                        <ul class="list-group">
+                                            <?php foreach ($album->getSongs() as $song): ?>
+                                                <!-- Repeat for each track -->
+                                                <li class="list-group-item d-flex align-items-center justify-content-between">
+                                                    <div class="track-info d-flex align-items-center">
+                                                        <button class="btn btn-sm play-button me-3">
+                                                            <i class="bi bi-play-circle"></i>
+                                                        </button>
+                                                        <div>
+                                                            <p class="mb-0"><?= $song->getName() ?></p></div>
+                                                    </div>
+                                                    <span class="text-muted"><?= $song->getLength() ?></span>
+                                                </li>
+                                            <?php endforeach; ?>
+                                        </ul>
+                                    </div>
                                 </ul>
 
                                 <p id="review" class="mt-3" style="display: none;">
