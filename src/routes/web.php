548cf50429123eca977b19b1974e2c3ddc8809d4
Dan-Sones <daniel.sones123@gmail.com>
2024-11-27 13:09:16 +0000

    Allow container to perform dependency injection with routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,38 +3,14 @@
 use S246109\BeatMagazine\Controllers\AlbumController;
 use S246109\BeatMagazine\Controllers\AlbumsController;
 use S246109\BeatMagazine\Controllers\ArtistController;
-use S246109\BeatMagazine\Factories\AlbumFactory;
-use S246109\BeatMagazine\Factories\ArtistFactory;
-use S246109\BeatMagazine\Factories\JournalistReviewFactory;
+use Slim\App;
 
-return function ($app) {
-    $app->get('/', function ($request, $response) {
-        require_once __DIR__ . '/../app/Controllers/HomeController.php';
-        $controller = new HomeController();
-        return $controller->index($request, $response);
-    });
+return function (App $app) {
+    $app->get('/', HomeController::class . ':index');
 
-    $app->get('/albums', function ($request, $response) use ($app) {
-        require_once __DIR__ . '/../app/Controllers/AlbumsController.php';
-        $albumFactory = $app->getContainer()->get(AlbumFactory::class);
-        $controller = new AlbumsController($albumFactory);
-        return $controller->index($request, $response);
-    });
+    $app->get('/albums', AlbumsController::class . ':index');
 
-    $app->get('/artist/{artistName}', function ($request, $response, $args) use ($app) {
-        require_once __DIR__ . '/../app/Controllers/AlbumController.php';
-        $artistFactory = $app->getContainer()->get(ArtistFactory::class);
-        $controller = new ArtistController($artistFactory);
-        return $controller->show($request, $response, $args);
-    });
+    $app->get('/artist/{artistName}', ArtistController::class . ':show');
 
-    $app->get('/artist/{artistName}/{albumName:.+}', function ($request, $response, $args) use ($app) {
-        require_once __DIR__ . '/../app/Controllers/AlbumController.php';
-        $albumFactory = $app->getContainer()->get(AlbumFactory::class);
-        $journalistReviewFactory = $app->getContainer()->get(JournalistReviewFactory::class);
-        $controller = new AlbumController($albumFactory, $journalistReviewFactory);
-        return $controller->show($request, $response, $args);
-    });
-
-
+    $app->get('/artist/{artistName}/{albumName:.+}', AlbumController::class . ':show');
 };
