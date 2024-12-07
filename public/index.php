5ca176cbaafebe05fa2d187405769c3dc8323f46
Dan-Sones <daniel.sones123@gmail.com>
2024-12-07 14:29:23 +0000

    re-structure project

--- public/index.php
+++ public/index.php
@@ -4,13 +4,13 @@ use DI\ContainerBuilder;
 use Dotenv\Dotenv;
 use Slim\Factory\AppFactory;
 
-require_once __DIR__ . '/../../config/config.php';
-require_once __DIR__ . '/../../vendor/autoload.php';
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+require_once __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
 $containerBuilder = new ContainerBuilder();
-$dependencies = require __DIR__ . '/../../config/dependencies.php';
+$dependencies = require __DIR__ . '/../config/dependencies.php';
 $dependencies($containerBuilder);
 $container = $containerBuilder->build();
 
@@ -22,8 +22,8 @@ $app->addErrorMiddleware(true, true, true);
 
 session_start();
 
-(require __DIR__ . '/../routes/web.php')($app);
-(require __DIR__ . '/../routes/api.php')($app);
+(require __DIR__ . '/../src/routes/web.php')($app);
+(require __DIR__ . '/../src/routes/api.php')($app);
 
 
 $app->run();
