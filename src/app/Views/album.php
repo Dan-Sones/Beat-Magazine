ec20610bf45dd8bfe0678ec028bd7ae408b3ed64
Dan-Sones <daniel.sones123@gmail.com>
2024-12-18 17:12:49 +0000

    improve album page accessibility

--- src/app/Views/album.php
+++ src/app/Views/album.php
@@ -2,7 +2,7 @@
 <?php include PRIVATE_PATH . '/src/app/components/review_editor_modal.php'; ?>
 
 <?php if (isset($album) && $album): ?>
-    <main class="album-wrapper">
+    <main class="album-wrapper" role="main">
         <div class="container-fluid">
             <div class="row justify-content-center">
                 <div class="col-xl-8 col-lg-10 col-md-10 col-sm-12">
@@ -11,31 +11,31 @@
                     <?php include PRIVATE_PATH . '/src/app/components/journalist_review.php'; ?>
                     <div class="container mt-3 mb-5">
                         <div class="text-center mb-3">
-                            <button id="tracklistButton" class="btn btn-primary">Tracklist</button>
-                            <button id="reviewButton" class="btn btn-secondary">Reviews</button>
+                            <button id="tracklistButton" class="btn btn-primary" aria-pressed="false"
+                                    aria-label="Show Tracklist">Tracklist
+                            </button>
+                            <button id="reviewButton" class="btn btn-secondary" aria-pressed="false"
+                                    aria-label="Show Reviews">Reviews
+                            </button>
                         </div>
                         <div id="tracklist" class="mt-3" style="display: none;">
                             <ul class="list-group">
                                 <?php if (sizeof($album->getSongs()) < 1): ?>
                                     <p>No tracks found.</p>
                                 <?php else: ?>
-
-                                <?php foreach ($album->getSongs() as $song): ?>
-                                    <li class="list-group-item d-flex align-items-center justify-content-between">
-                                        <div class="track-info d-flex align-items-center">
-                                            <button class="btn btn-sm play-button me-3">
-                                                <i class="bi bi-play-circle"></i>
-                                            </button>
-                                            <div>
-                                                <p class="mb-0"><?= $song->getName() ?></p></div>
-                                        </div>
-                                        <span class="text-muted"><?= $song->getLength() ?></span>
-                                    </li>
-                                <?php endforeach; ?>
+                                    <?php foreach ($album->getSongs() as $song): ?>
+                                        <li class="list-group-item d-flex align-items-center justify-content-between">
+                                            <div class="track-info d-flex align-items-center">
+                                                <div>
+                                                    <p class="mb-0"><?= htmlspecialchars($song->getName(), ENT_QUOTES, 'UTF-8') ?></p>
+                                                </div>
+                                            </div>
+                                            <span class="text-muted"><?= htmlspecialchars($song->getLength(), ENT_QUOTES, 'UTF-8') ?></span>
+                                        </li>
+                                    <?php endforeach; ?>
+                                <?php endif; ?>
                             </ul>
                         </div>
-                        <?php endif; ?>
-
 
                         <div class="container" id="user-reviews-container" style="display: none">
                             <?php include PRIVATE_PATH . '/src/app/components/create_user_review_card.php'; ?>
@@ -46,13 +46,10 @@
                             <?php else: ?>
                                 <p>No reviews available for this album</p>
                             <?php endif; ?>
-
-
                         </div>
                     </div>
 
                     <script>
-
                         const handleShowTracklist = () => {
                             localStorage.setItem("activeTab", "tracklist");
                             document.getElementById("tracklist").style.display = "block";
@@ -60,9 +57,11 @@
 
                             document.getElementById("tracklistButton").classList.remove("btn-secondary");
                             document.getElementById("tracklistButton").classList.add("btn-primary");
+                            document.getElementById("tracklistButton").setAttribute("aria-pressed", "true");
 
                             document.getElementById("reviewButton").classList.remove("btn-primary");
                             document.getElementById("reviewButton").classList.add("btn-secondary");
+                            document.getElementById("reviewButton").setAttribute("aria-pressed", "false");
                         }
 
                         const handleShowReviews = () => {
@@ -72,9 +71,11 @@
 
                             document.getElementById("tracklistButton").classList.remove("btn-primary");
                             document.getElementById("tracklistButton").classList.add("btn-secondary");
+                            document.getElementById("tracklistButton").setAttribute("aria-pressed", "false");
 
                             document.getElementById("reviewButton").classList.remove("btn-secondary");
                             document.getElementById("reviewButton").classList.add("btn-primary");
+                            document.getElementById("reviewButton").setAttribute("aria-pressed", "true");
                         }
 
                         document.addEventListener("DOMContentLoaded", function () {
@@ -82,15 +83,11 @@
                         });
 
                         document.getElementById("tracklistButton").addEventListener("click", handleShowTracklist);
-
                         document.getElementById("reviewButton").addEventListener("click", handleShowReviews);
                     </script>
-
-
                 </div>
             </div>
         </div>
-
     </main>
 
 <?php else: ?>
@@ -98,10 +95,9 @@
 <?php endif; ?>
 
     <script>
-        const albumIdGlobal = <?= $album->getAlbumID() ?>;
+        const albumIdGlobal = <?= htmlspecialchars($album->getAlbumID(), ENT_QUOTES, 'UTF-8') ?>;
     </script>
 
     <script src="/js/review_card.js"></script>
-
 
 <?php include 'includes/footer.php'; ?>
