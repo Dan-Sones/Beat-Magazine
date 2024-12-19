1f1c82f7c806adc53a83047879f3a38e4d642214
Dan-Sones <daniel.sones123@gmail.com>
2024-12-19 14:00:42 +0000

    Make Upgrade Page more accessible

--- src/app/Views/upgrade.php
+++ src/app/Views/upgrade.php
@@ -5,21 +5,22 @@
             <div class="row justify-content-center">
                 <div class="col-10 col-md-8 col-lg-6 col-sm-6">
                     <h2 class="mb-4 text-center">Upgrade Account</h2>
-                    <form id="loginForm" onsubmit="submitUpgradeForm(event)">
+                    <form id="loginForm" onsubmit="submitUpgradeForm(event)" role="form" aria-live="polite">
                         <p class="text-center">Are you sure you want to upgrade the account with the username
-                            "<?= $_SESSION['username'] ?>" to a Journalist Account?</p>
+                            "<?= htmlspecialchars($_SESSION['username'], ENT_QUOTES, 'UTF-8') ?>" to a Journalist
+                            Account?</p>
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password"
-                                   placeholder="Enter password" required>
+                            <input type="password" class="form-control" id="password" placeholder="Enter password"
+                                   required aria-label="Password">
                         </div>
                         <div class="d-flex justify-content-center">
-                            <button class="btn btn-success" id="submitLogin" type="submit">Submit</button>
+                            <button class="btn btn-success" id="submitLogin" type="submit" aria-label="Submit">Submit
+                            </button>
                         </div>
                     </form>
 
                     <script>
-
                         const submitUpgradeForm = async (event) => {
                             event.preventDefault();
 
@@ -33,18 +34,12 @@
                                 }),
                             });
                         }
-
-
                     </script>
-
-
                 </div>
             </div>
-
         </div>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
     </main>
 
 <?php include 'includes/footer.php'; ?>
