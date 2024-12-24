c4a7d1333830b206a6641742030037ec49777b40
Dan-Sones <daniel.sones123@gmail.com>
2024-12-24 17:41:44 +0000

    disable logs and change header

--- public/includes/header.php
+++ public/includes/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, maximum-scale=1.0, user-scalable=no">
-    <title>Beat Magazines</title>
+    <title>BeatMagazine.com</title>
     <link rel="stylesheet" href="/css/custom-bootstrap.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
