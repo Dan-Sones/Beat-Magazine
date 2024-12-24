b53df2f12c078c08c54ccb8c263bdb92b289f9ab
Dan-Sones <daniel.sones123@gmail.com>
2024-12-24 18:37:21 +0000

    Extract home js

--- src/app/Views/home.php
+++ src/app/Views/home.php
@@ -64,18 +64,7 @@
         <?php endif; ?>
     </div>
 
-    <script>
-        const UrlForAlbum = (artist, title) => {
-            const encodedArtist = encodeURIComponent(artist).replace(/%20/g, '+');
-            const encodedTitle = encodeURIComponent(title).replace(/%20/g, '+');
-            window.location.href = `/artist/${encodedArtist}/${encodedTitle}`;
-        }
-
-        const UrlForArtist = (artist) => {
-            const encodedArtist = encodeURIComponent(artist).replace(/%20/g, '+');
-            window.location.href = `/artist/${encodedArtist}`;
-        }
-
+    <script src="/js/home.js">
     </script>
 
 <?php include 'includes/footer.php'; ?>
