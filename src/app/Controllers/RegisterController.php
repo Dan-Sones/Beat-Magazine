251a39770d8088f8f6abe1c531880d47fdd0b6b6
Dan-Sones <daniel.sones123@gmail.com>
2024-12-29 12:07:04 +0000

    re-perform username and email address not taken checks on registration attempt

--- src/app/Controllers/RegisterController.php
+++ src/app/Controllers/RegisterController.php
@@ -42,35 +42,6 @@ class RegisterController
         return $response;
     }
 
-
-    public function isUsernameTaken(Request $request, Response $response, array $args): Response
-    {
-        $username = $request->getQueryParams()['username'] ?? '';
-
-        if (empty($username) || !is_string($username)) {
-            return $response->withStatus(400);
-        }
-
-        $isUsernameTaken = $this->userService->isUsernameTaken($username);
-
-        $response->getBody()->write(json_encode(['taken' => $isUsernameTaken]));
-        return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
-    }
-
-    public function isEmailTaken(Request $request, Response $response, array $args): Response
-    {
-        $email = $request->getQueryParams()['email'] ?? '';
-
-        if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            return $response->withStatus(400);
-        }
-
-        $isEmailTaken = $this->userService->isEmailTaken($email);
-
-        $response->getBody()->write(json_encode(['taken' => $isEmailTaken]));
-        return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
-    }
-
     public function verifyOTP(Request $request, Response $response, array $args): Response
     {
         $data = json_decode($request->getBody()->getContents(), true);
@@ -120,6 +91,15 @@ class RegisterController
         }
 
 
+        if ($this->userService->isUsernameTaken($username)) {
+            return $response->withStatus(409);
+        }
+
+        if ($this->userService->isEmailTaken($email)) {
+            return $response->withStatus(409);
+        }
+
+
         $confirmNumbers = preg_match_all('/[0-9]/', $password) >= 3;
         $confirmCapital = preg_match_all('/[A-Z]/', $password) >= 1;
         $confirmPunctuation = preg_match_all('/[!@#$%^&*()_+]/', $password) >= 1;
@@ -139,5 +119,33 @@ class RegisterController
         $_SESSION['google2faSecret'] = "";
 
         return $response->withStatus(201);
+    }
+
+    public function isUsernameTaken(Request $request, Response $response, array $args): Response
+    {
+        $username = $request->getQueryParams()['username'] ?? '';
+
+        if (empty($username) || !is_string($username)) {
+            return $response->withStatus(400);
+        }
+
+        $isUsernameTaken = $this->userService->isUsernameTaken($username);
+
+        $response->getBody()->write(json_encode(['taken' => $isUsernameTaken]));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+    }
+
+    public function isEmailTaken(Request $request, Response $response, array $args): Response
+    {
+        $email = $request->getQueryParams()['email'] ?? '';
+
+        if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return $response->withStatus(400);
+        }
+
+        $isEmailTaken = $this->userService->isEmailTaken($email);
+
+        $response->getBody()->write(json_encode(['taken' => $isEmailTaken]));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
     }
 }
