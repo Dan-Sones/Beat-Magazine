dcce18f4574b27198575149ad6879595a3d549dd
Dan-Sones <daniel.sones123@gmail.com>
2024-12-31 10:05:03 +0000

    Update album factory to support genres

--- src/app/Factories/AlbumFactory.php
+++ src/app/Factories/AlbumFactory.php
@@ -15,24 +15,24 @@ class AlbumFactory
         $this->db = $db;
     }
 
-
     public function getAlbumById(int $id): ?Album
     {
         $query = '
-    SELECT 
-        albums.id, 
-        albums.album_art, 
-        albums.name AS album_name, 
-        artists.name AS artist_name, 
-        albums.genre, 
-        albums.record_label, 
-        albums.average_user_rating, 
-        albums.journalist_rating, 
-        albums.release_date
-    FROM albums
-    INNER JOIN artists ON albums.artist_id = artists.id
-    WHERE albums.id = :album_id
-';
+        SELECT 
+            albums.id, 
+            albums.album_art, 
+            albums.name AS album_name, 
+            artists.name AS artist_name, 
+            genres.name AS genre, 
+            albums.record_label, 
+            albums.average_user_rating, 
+            albums.journalist_rating, 
+            albums.release_date
+        FROM albums
+        INNER JOIN artists ON albums.artist_id = artists.id
+        LEFT JOIN genres ON albums.genre_id = genres.id
+        WHERE albums.id = :album_id
+        ';
         $statement = $this->db->prepare($query);
         $statement->bindValue(':album_id', $id, PDO::PARAM_INT);
         $statement->execute();
@@ -51,30 +51,28 @@ class AlbumFactory
             $albumData['genre'],
             $albumData['record_label'],
             $albumData['release_date'],
-            [new Song("1", "Example", '2:11')] //We don't need the songs but the constructor requires it
+            [new Song("1", "Example", '2:11')]
         );
-
-
     }
-
 
     public function getAlbumByName(string $name, string $artistName): ?Album
     {
         $query = '
-    SELECT 
-        albums.id, 
-        albums.album_art, 
-        albums.name AS album_name, 
-        artists.name AS artist_name, 
-        albums.genre, 
-        albums.record_label, 
-        albums.average_user_rating, 
-        albums.journalist_rating, 
-        albums.release_date
-    FROM albums
-    INNER JOIN artists ON albums.artist_id = artists.id
-    WHERE albums.name = :album_name AND artists.name = :artist_name
-';
+        SELECT 
+            albums.id, 
+            albums.album_art, 
+            albums.name AS album_name, 
+            artists.name AS artist_name, 
+            genres.name AS genre, 
+            albums.record_label, 
+            albums.average_user_rating, 
+            albums.journalist_rating, 
+            albums.release_date
+        FROM albums
+        INNER JOIN artists ON albums.artist_id = artists.id
+        LEFT JOIN genres ON albums.genre_id = genres.id
+        WHERE albums.name = :album_name AND artists.name = :artist_name
+        ';
         $statement = $this->db->prepare($query);
         $statement->bindValue(':album_name', $name, PDO::PARAM_STR);
         $statement->bindValue(':artist_name', $artistName, PDO::PARAM_STR);
@@ -88,8 +86,7 @@ class AlbumFactory
 
         $songQuery = '
         SELECT id, name, length FROM songs WHERE album_id = :album_id
-    ';
-
+        ';
         $songStatement = $this->db->prepare($songQuery);
         $songStatement->bindValue(':album_id', $albumData['id'], PDO::PARAM_INT);
         $songStatement->execute();
@@ -115,20 +112,20 @@ class AlbumFactory
     public function getAllAlbums(): ?array
     {
         $query = '
-    SELECT 
-        albums.id,
-        albums.album_art, 
-        albums.name AS album_name, 
-        artists.name AS artist_name, 
-        albums.genre, 
-        albums.record_label, 
-        albums.average_user_rating, 
-        albums.journalist_rating, 
-        albums.release_date
-    FROM albums
-    INNER JOIN artists ON albums.artist_id = artists.id
-';
-
+        SELECT 
+            albums.id, 
+            albums.album_art, 
+            albums.name AS album_name, 
+            artists.name AS artist_name, 
+            genres.name AS genre, 
+            albums.record_label, 
+            albums.average_user_rating, 
+            albums.journalist_rating, 
+            albums.release_date
+        FROM albums
+        INNER JOIN artists ON albums.artist_id = artists.id
+        LEFT JOIN genres ON albums.genre_id = genres.id
+        ';
         $statement = $this->db->prepare($query);
         $statement->execute();
 
@@ -136,6 +133,7 @@ class AlbumFactory
             return null;
         }
 
+        $albums = [];
         while ($albumData = $statement->fetch(PDO::FETCH_ASSOC)) {
             $albums[] = new Album(
                 $albumData['id'],
@@ -145,7 +143,7 @@ class AlbumFactory
                 $albumData['genre'],
                 $albumData['record_label'],
                 $albumData['release_date'],
-                [new Song("1", "Example", '2:11')] //We don't need the songs but the constructor requires it
+                [new Song("1", "Example", '2:11')]
             );
         }
 
@@ -155,20 +153,21 @@ class AlbumFactory
     public function getAlbumsByArtistName(string $artistName): ?array
     {
         $query = '
-    SELECT 
-        albums.id,
-        albums.album_art, 
-        albums.name AS album_name, 
-        artists.name AS artist_name, 
-        albums.genre, 
-        albums.record_label, 
-        albums.average_user_rating, 
-        albums.journalist_rating, 
-        albums.release_date
-    FROM albums
-    INNER JOIN artists ON albums.artist_id = artists.id WHERE artists.name = :artist_name
-';
-
+        SELECT 
+            albums.id, 
+            albums.album_art, 
+            albums.name AS album_name, 
+            artists.name AS artist_name, 
+            genres.name AS genre, 
+            albums.record_label, 
+            albums.average_user_rating, 
+            albums.journalist_rating, 
+            albums.release_date
+        FROM albums
+        INNER JOIN artists ON albums.artist_id = artists.id
+        LEFT JOIN genres ON albums.genre_id = genres.id
+        WHERE artists.name = :artist_name
+        ';
         $statement = $this->db->prepare($query);
         $statement->bindValue(':artist_name', $artistName, PDO::PARAM_STR);
         $statement->execute();
@@ -177,6 +176,7 @@ class AlbumFactory
             return null;
         }
 
+        $albums = [];
         while ($albumData = $statement->fetch(PDO::FETCH_ASSOC)) {
             $albums[] = new Album(
                 $albumData['id'],
@@ -196,26 +196,24 @@ class AlbumFactory
     public function searchAlbums(string $search): array|null
     {
         $query = '
-        SELECT
-            albums.id,
-            albums.album_art,
-            albums.name AS album_name,
-            artists.name AS artist_name,
-            albums.genre,
-            albums.record_label,
-            albums.average_user_rating,
-            albums.journalist_rating,
+        SELECT 
+            albums.id, 
+            albums.album_art, 
+            albums.name AS album_name, 
+            artists.name AS artist_name, 
+            genres.name AS genre, 
+            albums.record_label, 
+            albums.average_user_rating, 
+            albums.journalist_rating, 
             albums.release_date
         FROM albums
         INNER JOIN artists ON albums.artist_id = artists.id
+        LEFT JOIN genres ON albums.genre_id = genres.id
         WHERE albums.name LIKE :searchOne OR artists.name LIKE :searchTwo
-    ';
-
+        ';
         $statement = $this->db->prepare($query);
-
         $statement->bindValue(':searchOne', '%' . $search . '%', PDO::PARAM_STR);
         $statement->bindValue(':searchTwo', '%' . $search . '%', PDO::PARAM_STR);
-
         $statement->execute();
 
         if ($statement->rowCount() === 0) {
@@ -242,20 +240,21 @@ class AlbumFactory
     public function getAlbumsByIdsMappedByIds(array $albumIds): array
     {
         $query = '
-    SELECT 
-        albums.id, 
-        albums.album_art, 
-        albums.name AS album_name, 
-        artists.name AS artist_name, 
-        albums.genre, 
-        albums.record_label, 
-        albums.average_user_rating, 
-        albums.journalist_rating, 
-        albums.release_date
-    FROM albums
-    INNER JOIN artists ON albums.artist_id = artists.id
-    WHERE albums.id = :album_id
-';
+        SELECT 
+            albums.id, 
+            albums.album_art, 
+            albums.name AS album_name, 
+            artists.name AS artist_name, 
+            genres.name AS genre, 
+            albums.record_label, 
+            albums.average_user_rating, 
+            albums.journalist_rating, 
+            albums.release_date
+        FROM albums
+        INNER JOIN artists ON albums.artist_id = artists.id
+        LEFT JOIN genres ON albums.genre_id = genres.id
+        WHERE albums.id = :album_id
+        ';
 
         $albums = [];
 
@@ -283,8 +282,5 @@ class AlbumFactory
         }
 
         return $albums;
-
-
     }
-
 }
