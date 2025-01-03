de4504395cdc5ddcfa8cce98813e65c888faafb9
Dan-Sones <daniel.sones123@gmail.com>
2025-01-03 09:47:22 +0000

    Update footer

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -1,11 +1,6 @@
 <footer class="bg-dark text-light py-3 mt-auto">
     <div class="container text-center">
-        <p class="mb-1">&copy; 2024 Beat Magazine. All rights reserved.</p>
-        <!--        <ul class="list-inline">-->
-        <!--            <li class="list-inline-item"><a href="#" class="text-secondary">Privacy Policy</a></li>-->
-        <!--            <li class="list-inline-item"><a href="#" class="text-secondary">Terms of Service</a></li>-->
-        <!--            <li class="list-inline-item"><a href="#" class="text-secondary">Contact Us</a></li>-->
-        <!--        </ul>-->
+        <p class="mb-1">&copy; 2025 Beat Magazine. All rights reserved. - This is a site purely for educational use</p>
     </div>
 </footer>
 
